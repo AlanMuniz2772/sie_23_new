b36acb1cf27c26a5042a5e365db4e478aec631fa
Alan <jorgeamr2772@gmail.com>
2023-11-22 08:56:56 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+   <h1 class="text-3xl font-bold text-blue-500">Tjjjexto azul</h1>
+</template>
+
+<style scoped>
+
+</style>
